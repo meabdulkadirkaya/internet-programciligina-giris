844a8efb21b38bd5bee30a0cfa23ca453e472046
Abdulkadir Kaya <37632855+meabdulkadirkaya@users.noreply.github.com>
2024-12-11 05:19:43 +0300

    Update and rename hafta9.php to cok-boyutlu-dizi.php

---
+++ hafta9/cok-boyutlu-dizi.php
@@ -0,0 +1,15 @@
+<?php
+
+    $ogrenci = array();
+    $ogrenci[0] = array("Abdulkadir", "Amasya");
+    $ogrenci[1] = array("Sude Naz", "Amasya");
+    $ogrenci[2] = array("Dilara", "Amasya");
+    $ogrenci[2] = array("Melike", "Amasya");
+    $ogrenci[2] = array("Miray", "Amasya");
+    $ogrenci[2] = array("Mehmet Fatih", "Amasya");
+
+    echo "<pre>";
+    print_r($ogrenci);
+    echo "</pre>";
+
+?>
