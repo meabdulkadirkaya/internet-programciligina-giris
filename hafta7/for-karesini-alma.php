7075b66890d2ba5c53cb49b1f14bfc843b89887e
Abdulkadir Kaya <37632855+meabdulkadirkaya@users.noreply.github.com>
2024-12-11 05:14:35 +0300

    Create for-karesini-alma.php

---
+++ hafta7/for-karesini-alma.php
@@ -0,0 +1,9 @@
+<?php
+
+    for ($i=1; $i<=10; $i++) {
+        echo $i." Sayısının Karesi: " .$i*$i. "<br>";
+        echo $i." Sayısının Karesi: " .($i**2). "<br>";
+        echo $i." Sayısının Karesi: " .pow($i,2). "<br>";
+    }
+
+?>
