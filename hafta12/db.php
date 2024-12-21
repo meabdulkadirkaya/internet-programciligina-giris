4efc738ad450450ec9c6ce018f41bec310217083
Abdulkadir Kaya <37632855+meabdulkadirkaya@users.noreply.github.com>
2024-12-21 20:02:37 +0300

    Create db.php

---
+++ hafta12/db.php
@@ -0,0 +1,10 @@
+<?php
+
+    try {
+        $db = new PDO("mysql:host=localhost; dbname=ornek; charset=utf8","root", "");
+    }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+
+?>
